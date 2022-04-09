7272e9941f9f5b48bd6870a875b338309c394a82
erikcorrea444@gmail.com <erikcorrea444@gmail.com>
2022-04-09 12:03:55 -0400

    setting up chat page

---
+++ src/components/Contacts.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Logo from '../assets/logo.png';
+
+export default function Contacts({contacts, currentUser}) {
+  return (
+    <div>Contacts</div>
+  )
+}
